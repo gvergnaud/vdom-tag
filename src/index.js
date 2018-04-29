219f1d02c6f00ae115c2087a4f396c43e7d01dbb
gvergnaud <gabriel.vergnaud@hetic.net>
2018-04-29 16:27:56 +0200

    create vdom-tag package

--- src/index.js
+++ src/index.js
@@ -1,1 +1,417 @@
-export default () => 'Hello world!'
+import { flatMap, escapeHTML, isEmpty } from './utils'
+
+const unionType = (() => {
+  let i = 0
+  return types => types.reduce((acc, type) => ({ ...acc, [type]: i++ }), {})
+})()
+
+export const State = unionType([
+  'Text',
+  'Tag',
+  'ClosingTag',
+  'Attr',
+  'AttrKey',
+  'AttrValue',
+  'AttrValueSingleQuote',
+  'AttrValueDoubleQuote'
+])
+
+export const Token = unionType([
+  'Text',
+  'OpenTag',
+  'AttributeName',
+  'AttributeValue',
+  'CloseTag',
+  'SelfCloseTag',
+  'ClosingTag',
+  'VTree'
+])
+
+export const Tree = unionType(['Node', 'VTree', 'Text'])
+
+export function tokenizer(str, initialState = State.Text, initialAcc = '') {
+  let tokens = []
+  let state = initialState
+  let acc = initialAcc
+
+  for (let i = 0; i < str.length; i++) {
+    const c = str[i]
+    switch (state) {
+      case State.Text:
+        if (c === '<') {
+          if (!isEmpty(acc)) {
+            tokens.push({ type: Token.Text, value: escapeHTML(acc) })
+          }
+          acc = ''
+          state = State.Tag
+        } else {
+          acc += c
+        }
+        continue
+
+      case State.Tag:
+        if (/\s/.test(c)) {
+          tokens.push({ type: Token.OpenTag, value: acc })
+          acc = ''
+          state = State.Attr
+        } else if (c === '>') {
+          tokens.push({ type: Token.OpenTag, value: acc })
+          tokens.push({ type: Token.CloseTag })
+          acc = ''
+          state = State.Text
+        } else if (c === '/' && acc === '') {
+          state = State.ClosingTag
+        } else {
+          acc += c
+        }
+        continue
+
+      case State.ClosingTag:
+        if (c === '>') {
+          tokens.push({ type: Token.ClosingTag, value: acc })
+          acc = ''
+          state = State.Text
+        } else {
+          acc += c
+        }
+        continue
+
+      case State.Attr:
+        if (c === '>') {
+          if (acc === '/') tokens.push({ type: Token.SelfCloseTag })
+          else tokens.push({ type: Token.CloseTag })
+          acc = ''
+          state = State.Text
+        } else if (/\s/.test(c)) {
+        } else if (c === '/') {
+          acc += c
+        } else {
+          acc += c
+          state = State.AttrKey
+        }
+        continue
+
+      case State.AttrKey:
+        if (c === '=') {
+          // The attr name could be dynamic, so it would be in a previous fragment
+          if (acc !== '') tokens.push({ type: Token.AttributeName, value: acc })
+          acc = ''
+          state = State.AttrValue
+        } else if (/\s/.test(c)) {
+          if (acc !== '') tokens.push({ type: Token.AttributeName, value: acc })
+          acc = ''
+          state = State.Attr
+        } else {
+          acc += c
+        }
+        continue
+
+      case State.AttrValue:
+        if (/\s/.test(c)) {
+          tokens.push({ type: Token.AttributeValue, value: acc })
+          acc = ''
+          state = State.Attr
+        } else if (c === '>') {
+          tokens.push({ type: Token.AttributeValue, value: acc })
+          tokens.push({ type: Token.CloseTag })
+          acc = ''
+          state = State.Text
+        } else if (acc === '' && c === "'") {
+          state = State.AttrValueSingleQuote
+        } else if (acc === '' && c === '"') {
+          state = State.AttrValueDoubleQuote
+        } else {
+          acc += c
+        }
+        continue
+
+      case State.AttrValueSingleQuote:
+        if (c === "'") {
+          tokens.push({ type: Token.AttributeValue, value: acc })
+          acc = ''
+          state = State.Attr
+        } else {
+          acc += c
+        }
+        continue
+
+      case State.AttrValueDoubleQuote:
+        if (c === '"') {
+          tokens.push({ type: Token.AttributeValue, value: acc })
+          acc = ''
+          state = State.Attr
+        } else {
+          acc += c
+        }
+        continue
+    }
+  }
+
+  return [tokens, state, acc]
+}
+
+const parseObjectAttrs = obj => [
+  flatMap(
+    ([name, value]) =>
+      !name.trim()
+        ? []
+        : [
+            { type: Token.AttributeName, value: name },
+            { type: Token.AttributeValue, value }
+          ],
+    Object.entries(obj)
+  ),
+  State.Attr
+]
+
+const parseStringAttrs = str =>
+  str
+    .split(/\s+/)
+    .filter(attr => attr.trim())
+    .reduce(
+      ([tokens], attr) => {
+        const [name, value] = attr.split('=')
+
+        if (value) {
+          return [
+            tokens.concat([
+              { type: Token.AttributeName, value: name },
+              { type: Token.AttributeValue, value }
+            ]),
+            State.Attr
+          ]
+        }
+
+        return [
+          tokens.concat([{ type: Token.AttributeName, value: name }]),
+          State.AttrKey
+        ]
+      },
+      [[], State.Attr]
+    )
+
+export function variableTokenizer(
+  variable,
+  initialState = Token.Text,
+  initialAcc = ''
+) {
+  if (variable === undefined) return [[], initialState, initialAcc]
+
+  let tokens = []
+  let state = initialState
+  let acc = initialAcc
+
+  switch (state) {
+    case State.Text:
+      if (typeof variable === 'object') {
+        if (!isEmpty(acc)) {
+          tokens.push({ type: Token.Text, value: escapeHTML(acc) })
+        }
+
+        tokens.push({ type: Token.VTree, value: variable })
+
+        acc = ''
+      } else {
+        if (!isEmpty(acc)) {
+          tokens.push({ type: Token.Text, value: escapeHTML(acc) })
+        }
+
+        if (!isEmpty(variable)) {
+          tokens.push({
+            type: Token.Text,
+            value: escapeHTML(variable.toString())
+          })
+        }
+
+        acc = ''
+      }
+      break
+
+    case State.Tag:
+      if (acc !== '') {
+        acc += variable
+      } else {
+        acc = variable
+      }
+      break
+
+    case State.ClosingTag:
+      if (acc !== '') {
+        acc += variable
+      } else {
+        acc = variable
+      }
+      break
+
+    case State.Attr:
+      if (typeof variable === 'object') {
+        [tokens, state] = parseObjectAttrs(variable)
+      } else if (typeof variable === 'string') {
+        [tokens, state] = parseStringAttrs(variable)
+      } else {
+        throw new Error(
+          `Variable ${variable} of type '${typeof variable}' is not supported in the State.Attr context`
+        )
+      }
+      break
+
+    case State.AttrKey:
+      throw new Error(
+        `Variable ${variable} of type '${typeof variable}' is not supported in the State.AttrKey context`
+      )
+
+    case State.AttrValue:
+      if (acc !== '') {
+        acc += variable
+      } else {
+        acc = variable
+      }
+      break
+
+    case State.AttrValueSingleQuote:
+      if (acc !== '') {
+        acc += variable
+      } else {
+        acc = variable
+      }
+      break
+
+    case State.AttrValueDoubleQuote:
+      if (acc !== '') {
+        acc += variable
+      } else {
+        acc = variable
+      }
+      break
+  }
+
+  return [tokens, state, acc]
+}
+
+export const tokenizerTag = (strings, ...variables) => {
+  let tokens = []
+  let state = State.Text
+  let acc = ''
+
+  for (let i = 0; i < strings.length; i++) {
+    const string = strings[i]
+    const variable = variables[i]
+    const [stringTokens, stringState, stringAcc] = tokenizer(string, state, acc)
+    const [variableTokens, variableState, variableAcc] = variableTokenizer(
+      variable,
+      stringState,
+      stringAcc
+    )
+
+    tokens = tokens.concat(stringTokens).concat(variableTokens)
+    state = variableState
+    acc = variableAcc
+  }
+
+  return tokens
+}
+
+const NAME_INDEX = 0
+const ATTR_INDEX = 1
+const CHILDREN_INDEX = 2
+
+const applyH = (h, treeParts) =>
+  treeParts.map(treePart => {
+    switch (treePart.type) {
+      case Tree.Node:
+        return h(
+          treePart.value[NAME_INDEX],
+          treePart.value[ATTR_INDEX],
+          applyH(h, treePart.value[CHILDREN_INDEX])
+        )
+      case Tree.VTree:
+      case Tree.Text:
+        return treePart.value
+    }
+  })
+
+// buildTree
+//  :: [Token]
+//  -> Int
+//  -> [[Tree], Int]
+export function buildTree(tokens, initialIndex = 0) {
+  let treeParts = []
+  let i = initialIndex
+
+  while (i < tokens.length) {
+    const token = tokens[i]
+    const lastTreePart = treeParts[treeParts.length - 1]
+    const currentNode =
+      lastTreePart && lastTreePart.type === Tree.Node
+        ? lastTreePart.value
+        : null
+
+    switch (token.type) {
+      case Token.OpenTag:
+        treeParts.push({ type: Tree.Node, value: [token.value, {}, []] })
+        i++
+        break
+
+      case Token.Text:
+        treeParts.push({ type: Tree.Text, value: token.value })
+        i++
+        break
+
+      case Token.VTree:
+        treeParts.push(
+          ...(!Array.isArray(token.value)
+            ? [{ type: Tree.VTree, value: token.value }]
+            : token.value.map(value => ({ type: Tree.VTree, value })))
+        )
+        i++
+        break
+
+      case Token.AttributeName:
+        if (currentNode) {
+          if (tokens[i + 1] && tokens[i + 1].type === Token.AttributeValue) {
+            currentNode[ATTR_INDEX][token.value] = tokens[i + 1].value
+            i += 2
+          } else {
+            currentNode[ATTR_INDEX][token.value] = true
+            i++
+          }
+        } else {
+          i++
+        }
+        break
+
+      case Token.AttributeValue:
+        // Nothing, can't happen since it's always following a Token.AttributeName
+        // which handle this case.
+        break
+
+      case Token.CloseTag: // eslint-disable-line
+        if (currentNode) {
+          let [children, nextIndex] = buildTree(tokens, i + 1)
+          currentNode[CHILDREN_INDEX] = children
+          i = nextIndex
+        } else {
+          i++
+        }
+        break
+
+      case Token.SelfCloseTag:
+        i++
+        break
+
+      case Token.ClosingTag:
+        return [treeParts, i + 1]
+    }
+  }
+
+  return [treeParts, i]
+}
+
+const unwrapIfOnlyValue = xs => (xs.length === 1 ? xs[0] : xs)
+
+export default function createParser(h) {
+  return (...args) => {
+    const [tree] = buildTree(tokenizerTag(...args))
+    return unwrapIfOnlyValue(applyH(h, tree))
+  }
+}
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,337 @@
-import index from '../src'
+import createParser, {
+  buildTree,
+  tokenizer,
+  variableTokenizer,
+  tokenizerTag,
+  Token,
+  State,
+  Tree
+} from '../src'
 
-describe('index', () => {
+const h = (name, attrs, children) => ({ name, attrs, children })
+const html = createParser(h)
+
+describe('tokenizer', () => {
+  it('should parse attrs', () => {
+    expect(tokenizer('<div class="cool">', State.Text)).toEqual([
+      [
+        { type: Token.OpenTag, value: 'div' },
+        { type: Token.AttributeName, value: 'class' },
+        { type: Token.AttributeValue, value: 'cool' },
+        { type: Token.CloseTag }
+      ],
+      State.Text,
+      ''
+    ])
+  })
+
+  it('should parse attributes without values', () => {
+    expect(tokenizer(' autofocus ', State.Attr)).toEqual([
+      [{ type: Token.AttributeName, value: 'autofocus' }],
+      State.Attr,
+      ''
+    ])
+  })
+
+  it('should parse attributes without quotes', () => {
+    expect(tokenizer(`<p class=yes>`, State.Text)).toEqual([
+      [
+        { type: Token.OpenTag, value: 'p' },
+        { type: Token.AttributeName, value: 'class' },
+        { type: Token.AttributeValue, value: 'yes' },
+        { type: Token.CloseTag }
+      ],
+      State.Text,
+      ''
+    ])
+  })
+
+  it('should parse selfclosing tags', () => {
+    expect(tokenizer(' />', State.Attr)).toEqual([
+      [{ type: Token.SelfCloseTag }],
+      State.Text,
+      ''
+    ])
+  })
+
+  it('should parse text', () => {
+    expect(tokenizer('<p>hello hello hello</p>', State.Text)).toEqual([
+      [
+        { type: Token.OpenTag, value: 'p' },
+        { type: Token.CloseTag },
+        { type: Token.Text, value: 'hello hello hello' },
+        { type: Token.ClosingTag, value: 'p' }
+      ],
+      State.Text,
+      ''
+    ])
+  })
+
   it('should work', () => {
-    expect(index()).toEqual('Hello world!')
+    expect(
+      tokenizer(
+        '<div class="cool"><input autofocus value="yes" /></div>',
+        State.Text
+      )
+    ).toEqual([
+      [
+        { type: Token.OpenTag, value: 'div' },
+        { type: Token.AttributeName, value: 'class' },
+        { type: Token.AttributeValue, value: 'cool' },
+        { type: Token.CloseTag },
+        { type: Token.OpenTag, value: 'input' },
+        { type: Token.AttributeName, value: 'autofocus' },
+        { type: Token.AttributeName, value: 'value' },
+        { type: Token.AttributeValue, value: 'yes' },
+        { type: Token.SelfCloseTag },
+        { type: Token.ClosingTag, value: 'div' }
+      ],
+      State.Text,
+      ''
+    ])
+  })
+})
+
+describe('variableTokenizer', () => {
+  it('should understand object as attributes', () => {
+    expect(
+      variableTokenizer({ class: 'cool', autofocus: true }, State.Attr)
+    ).toEqual([
+      [
+        { type: Token.AttributeName, value: 'class' },
+        { type: Token.AttributeValue, value: 'cool' },
+        { type: Token.AttributeName, value: 'autofocus' },
+        { type: Token.AttributeValue, value: true }
+      ],
+      State.Attr,
+      ''
+    ])
+  })
+})
+
+describe('tokenizerTag', () => {
+  it('should successfully parse dynamic attr values', () => {
+    expect(tokenizerTag`<div class="${'cool'}">`).toEqual([
+      { type: Token.OpenTag, value: 'div' },
+      { type: Token.AttributeName, value: 'class' },
+      { type: Token.AttributeValue, value: 'cool' },
+      { type: Token.CloseTag }
+    ])
+
+    expect(tokenizerTag`<Custom list="${[1, 2, 3]}">`).toEqual([
+      { type: Token.OpenTag, value: 'Custom' },
+      { type: Token.AttributeName, value: 'list' },
+      { type: Token.AttributeValue, value: [1, 2, 3] },
+      { type: Token.CloseTag }
+    ])
+  })
+
+  it('should successfully parse dynamic tag names', () => {
+    expect(tokenizerTag`<${'input'} />`).toEqual([
+      { type: Token.OpenTag, value: 'input' },
+      { type: Token.SelfCloseTag }
+    ])
+
+    const Custom = () => {}
+    expect(tokenizerTag`<${Custom} />`).toEqual([
+      { type: Token.OpenTag, value: Custom },
+      { type: Token.SelfCloseTag }
+    ])
+  })
+
+  it('should be able to parse dynamic attr names', () => {
+    expect(tokenizerTag`<input type="checkbox" ${'checked'}="yes" />`).toEqual([
+      { type: Token.OpenTag, value: 'input' },
+      { type: Token.AttributeName, value: 'type' },
+      { type: Token.AttributeValue, value: 'checkbox' },
+      { type: Token.AttributeName, value: 'checked' },
+      { type: Token.AttributeValue, value: 'yes' },
+      { type: Token.SelfCloseTag }
+    ])
+  })
+
+  it('should be able to parse dynamic attr values without quotes', () => {
+    expect(tokenizerTag`<div class=${'yo'}>`).toEqual([
+      { type: Token.OpenTag, value: 'div' },
+      { type: Token.AttributeName, value: 'class' },
+      { type: Token.AttributeValue, value: 'yo' },
+      { type: Token.CloseTag }
+    ])
+
+    expect(tokenizerTag`<input type=${'checkbox'} />`).toEqual([
+      { type: Token.OpenTag, value: 'input' },
+      { type: Token.AttributeName, value: 'type' },
+      { type: Token.AttributeValue, value: 'checkbox' },
+      { type: Token.SelfCloseTag }
+    ])
+  })
+
+  it('should work', () => {
+    expect(
+      tokenizerTag`
+          <div class="${'cool'}">
+            <${'input'} autofocus ${'value'}="yes" />
+            Cool
+          </div>
+      `
+    ).toEqual([
+      { type: Token.OpenTag, value: 'div' },
+      { type: Token.AttributeName, value: 'class' },
+      { type: Token.AttributeValue, value: 'cool' },
+      { type: Token.CloseTag },
+      { type: Token.OpenTag, value: 'input' },
+      { type: Token.AttributeName, value: 'autofocus' },
+      { type: Token.AttributeName, value: 'value' },
+      { type: Token.AttributeValue, value: 'yes' },
+      { type: Token.SelfCloseTag },
+      { type: Token.Text, value: '\n            Cool\n          ' },
+      { type: Token.ClosingTag, value: 'div' }
+    ])
+  })
+})
+
+describe('buildTree', () => {
+  it('should transform tokens into a virtalDOM tree', () => {
+    expect(
+      buildTree([
+        { type: Token.OpenTag, value: 'div' },
+        { type: Token.AttributeName, value: 'class' },
+        { type: Token.AttributeValue, value: 'cool' },
+        { type: Token.CloseTag },
+        { type: Token.OpenTag, value: 'input' },
+        { type: Token.AttributeName, value: 'autofocus' },
+        { type: Token.AttributeName, value: 'value' },
+        { type: Token.AttributeValue, value: 'yes' },
+        { type: Token.SelfCloseTag },
+        { type: Token.Text, value: 'Cool' },
+        { type: Token.OpenTag, value: 'div' },
+        { type: Token.CloseTag },
+        { type: Token.OpenTag, value: 'p' },
+        { type: Token.CloseTag },
+        { type: Token.Text, value: 'coucou' },
+        { type: Token.ClosingTag, value: 'p' },
+        { type: Token.OpenTag, value: 'p' },
+        { type: Token.CloseTag },
+        { type: Token.Text, value: 'pouet' },
+        { type: Token.ClosingTag, value: 'p' },
+        { type: Token.ClosingTag, value: 'div' },
+        { type: Token.ClosingTag, value: 'div' }
+      ])
+    ).toEqual([
+      [
+        {
+          type: Tree.Node,
+          value: [
+            'div',
+            { class: 'cool' },
+            [
+              {
+                type: Tree.Node,
+                value: ['input', { autofocus: true, value: 'yes' }, []]
+              },
+              { type: Tree.Text, value: 'Cool' },
+              {
+                type: Tree.Node,
+                value: [
+                  'div',
+                  {},
+                  [
+                    {
+                      type: Tree.Node,
+                      value: ['p', {}, [{ type: Tree.Text, value: 'coucou' }]]
+                    },
+                    {
+                      type: Tree.Node,
+                      value: ['p', {}, [{ type: Tree.Text, value: 'pouet' }]]
+                    }
+                  ]
+                ]
+              }
+            ]
+          ]
+        }
+      ],
+      22
+    ])
+  })
+})
+
+describe('createParser', () => {
+  it('should successfully parse complexe dom', () => {
+    expect(html`
+      <div class="${'cool'}">
+        <${'input'} autofocus ${'value'}="yes" />
+        <div>
+          Cool
+          <p>Hello</p>
+          <p>Can you dig it</p>
+        </div>
+      </div>
+    `).toEqual(
+      h('div', { class: 'cool' }, [
+        h('input', { autofocus: true, value: 'yes' }, []),
+        h('div', {}, [
+          '\n          Cool\n          ',
+          h('p', {}, ['Hello']),
+          h('p', {}, ['Can you dig it'])
+        ])
+      ])
+    )
+  })
+
+  it('should succefully parse nested calls of html', () => {
+    expect(html`
+        <div>
+          ${html`<p>coucou</p>`}
+        </div>
+      `).toEqual(h('div', {}, [h('p', {}, ['coucou'])]))
+  })
+
+  it('should succefully parse nested calls of html surrounded by text', () => {
+    expect(html`
+        <div>oh ${html`<p>coucou</p>`} yeah!</div>
+      `).toEqual(h('div', {}, ['oh ', h('p', {}, ['coucou']), ' yeah!']))
+  })
+
+  it('should ignore empty variables', () => {
+    expect(html`
+      <div ${''} />
+    `).toEqual(h('div', {}, []))
+  })
+
+  it('should succefully parse lists of html', () => {
+    expect(html`
+      <div>
+        ${[1, 2, 3].map(x => html`<p>n° ${x}</p>`)}
+      </div>
+    `).toEqual(
+      h('div', {}, [
+        h('p', {}, ['n° ', '1']),
+        h('p', {}, ['n° ', '2']),
+        h('p', {}, ['n° ', '3'])
+      ])
+    )
+  })
+
+  it('should succefully parse custom components', () => {
+    const Custom = () => {}
+    expect(html`
+      <${Custom} />
+    `).toEqual(h(Custom, {}, []))
+  })
+
+  it('should succefully parse lists of custom components', () => {
+    const Custom = () => {}
+    expect(html`
+      <div>
+        ${[1, 2, 3].map(x => html`<${Custom} id=${x} />`)}
+      </div>
+    `).toEqual(
+      h('div', {}, [
+        h(Custom, { id: 1 }, []),
+        h(Custom, { id: 2 }, []),
+        h(Custom, { id: 3 }, [])
+      ])
+    )
   })
 })
